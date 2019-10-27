67bd0639808f5a4fff3bd69b4ced36deca592664
Yu TaeWoong <ytw2854@gmail.com>
2019-10-27 12:09:15 +0900

    single crawler complete

--- data_collect/cgv_collect.py
+++ data_collect/cgv_collect.py
@@ -5,7 +5,6 @@ from bs4 import BeautifulSoup as bs
 import requests
 import time
 from traceback import format_exc
-from multiprocessing import Process
 import glob
 
 dir = os.path.dirname(__file__)
@@ -63,71 +62,101 @@ def page_worker_crawler(scene_length, page_number):
     # all movie find in cgv movie finder
     driver.find_element_by_xpath("//*[@class='round inred']").click()
 
+    checker = False
+
     for i in range(scene_length):
+        for j in range(page_number):
+            try:
+                if checker == True:
+                    page_idx = driver.find_element_by_xpath("//*[@class='paging']")\
+                        .find_elements_by_tag_name("li")[j]
+                    page_idx.find_element_by_tag_name("a").click()
+                else:
+                    checker = True
+            except:
+                print(format_exc())
 
-        try:
-            page_idx = driver.find_element_by_xpath("//*[@class='paging']")\
-                .find_element_by_xpath("//*[@a=" + str((i + 1) * page_number) + "]")
-            page_idx.find_element_by_xpath("a").click()
-        except:
-            print(format_exc())
-
-        # all movie info html component get
-        html = driver.page_source
-        soup = bs(html, 'html.parser')
-        movies_list = soup.find("div", {"class": "sect-search-chart"}).findAll("ol")
+            # all movie info html component get
+            html = driver.page_source
+            soup = bs(html, 'html.parser')
+            movies_list = soup.find("div", {"class": "sect-search-chart"}).findAll("ol")
 
 
-        for x in movies_list:
-            for y in x.findAll("li"):
-                try:
-                    movie = y.find("div", {"class": "box-contents"}).a
-                    each_movie_link = movie['href']
-                    each_movie_info = requests.get("http://www.cgv.co.kr" + each_movie_link)
-                    html = each_movie_info.text
-                    soup_movie = bs(html, 'html.parser')
+            for x in movies_list:
+                for y in x.findAll("li"):
+                    try:
+                        producer = ""
+                        actor_and_actress = ""
 
-                    movie_info = soup_movie.find("div", {"class": "box-contents"})
-                    title = movie_info.find("div", {"class": "title"}).strong.text
-                    reserve_percent = movie_info.find("div", {"class": "score"}).find("strong", {"class": "percent"}).span.text
-                    audience = movie_info.find("div", {"class": "score"}).find("span", {"class": "view"}).em.text
-                    cast = movie_info.find("div", {"class": "spec"}).find("dl")
+                        movie = y.find("div", {"class": "box-contents"}).a
+                        each_movie_link = movie['href']
+                        each_movie_info = requests.get("http://www.cgv.co.kr" + each_movie_link)
+                        html = each_movie_info.text
+                        soup_movie = bs(html, 'html.parser')
 
-                    director_text = [cast.findAll("dd")[idx].text for idx, z in enumerate(cast.findAll("dt")) if '감독' in z.text][0]
-                    producer_text = [cast.findAll("dd")[idx].text for idx, z in enumerate(cast.findAll("dt")) if '프로듀서' in z.text][0]
-                    actor_and_actress_text = [cast.findAll("dd")[idx].text for idx, z in enumerate(cast.findAll("dt")) if '배우' in z.text][0]
+                        movie_info = soup_movie.find("div", {"class": "box-contents"})
+                        title = movie_info.find("div", {"class": "title"}).strong.text
+                        reserve_percent = movie_info.find("div", {"class": "score"}).find("strong", {"class": "percent"}).span.text
+                        audience = movie_info.find("div", {"class": "score"}).find("span", {"class": "view"}).em.text
+                        cast = movie_info.find("div", {"class": "spec"}).find("dl")
 
-                    director = ''.join([z for z in director_text if ord('가') <= ord(z) <= ord('힣') or
-                                 ord(z) == ord(',') or ord('A') <= ord(z) <= ord('z')]).replace(',', '_')
-                    producer = ''.join([z for z in producer_text if ord('가') <= ord(z) <= ord('힣') or
-                                 ord(z) == ord(',') or ord('A') <= ord(z) <= ord('z')]).replace(',', '_')
-                    actor_and_actress = ''.join([z for z in actor_and_actress_text if ord('가') <= ord(z) <= ord('힣') or
-                                 ord(z) == ord(',') or ord('A') <= ord(z) <= ord('z')]).replace(',', '_')
+                        try:
+                            director_text = [cast.findAll("dd")[idx].text for idx, z in enumerate(cast.findAll("dt")) if '감독' in z.text][0]
+                            director = ''.join([z for z in director_text if ord('가') <= ord(z) <= ord('힣') or ord(z) == ord(',')])
+                        except:
+                            director = ""
 
-                    genre_text = [z.text for idx, z in enumerate(cast.findAll("dt")) if '장르' in z.text][0]
-                    genre = ''.join([z for z in genre_text if ord('가') <= ord(z) <= ord('힣') or
-                                 ord(z) == ord(',') or ord('A') <= ord(z) <= ord('z')]).replace(',', '_')
+                        producer_check = 0
+                        producer_text = [cast.findAll("dd")[idx].text for idx, z in enumerate(cast.findAll("dt")) if '프로듀서' in z.text]
+                        if len(producer_text) != 0:
+                            producer = ''.join([z for z in producer_text[0] if ord('가') <= ord(z) <= ord('힣') or ord(z) == ord(',')])
+                            if producer == "":
+                                producer_text = [cast.findAll("dd", {"class": "on"})[producer_check].text for idx, z in enumerate(cast.findAll("dt")) if '프로듀서' in z.text]
+                                if len(producer_text) != 0:
+                                    producer = ''.join([z for z in producer_text[0] if ord('가') <= ord(z) <= ord('힣') or ord(z) == ord(',')])
+                                    producer_check = 1
+                                else:
+                                    producer = ""
 
-                    release_date = cast.findAll("dd")[-1].text
+                        actor_and_actress_text = [cast.findAll("dd")[idx].text for idx, z in enumerate(cast.findAll("dt")) if '배우' in z.text]
+                        if len(actor_and_actress_text) != 0:
+                            actor_and_actress = ''.join([z for z in actor_and_actress_text[0] if ord('가') <= ord(z) <= ord('힣') or ord(z) == ord(',')])
+                            if actor_and_actress == "":
+                                actor_and_actress_text = [cast.findAll("dd", {"class": "on"})[producer_check].text for idx, z in enumerate(cast.findAll("dt")) if '배우' in z.text]
+                                if len(actor_and_actress_text) != 0:
+                                    actor_and_actress = ''.join([z for z in actor_and_actress_text[0] if ord('가') <= ord(z) <= ord('힣') or ord(z) == ord(',')])
+                                else:
+                                    actor_and_actress = ""
 
-                    recorder("movies_info", str(i + 1) + "_" + str(page_number) + ".csv",
-                             title + "," + reserve_percent + "," + audience + "," +
-                             director + "," + producer + "," + actor_and_actress +
-                             "," + genre + "," + release_date + "\n")
+                        try:
+                            genre_text = [z.text for idx, z in enumerate(cast.findAll("dt")) if '장르' in z.text][0]
+                            genre = ''.join([z for z in genre_text if ord('가') <= ord(z) <= ord('힣') or
+                                         ord(z) == ord(',')]).replace('장르', '')
+                        except:
+                            genre = ""
 
-                    print(title)
-                    print(reserve_percent)
-                    print(audience)
-                    print(director)
-                    print(producer)
-                    print(actor_and_actress)
-                    print(genre)
-                    print(release_date)
-                except:
-                    print(format_exc())
+                        release_date = cast.findAll("dd")[-1].text
 
-        time.sleep(1)
+                        recorder("movies_info", str(i+1) + "_" + str(j+1) + ".tsv",
+                                 title + "\t" + reserve_percent + "\t" + audience + "\t" +
+                                 director + "\t" + producer + "\t" + actor_and_actress +
+                                 "\t" + genre + "\t" + release_date + "\n")
+
+                        print(title)
+                        print(reserve_percent)
+                        print(audience)
+                        print(director)
+                        print(producer)
+                        print(actor_and_actress)
+                        print(genre)
+                        print(release_date)
+                    except:
+                        print(format_exc())
+
+                time.sleep(1)
+            time.sleep(2)
         driver.find_element_by_xpath("//*[@class='btn-paging next']").click()
+        checker = False
 
     driver.close()
 
@@ -152,26 +181,16 @@ if __name__ == '__main__':
 
     driver.close()
 
-    # multi crawling
-    proc_list = []
-    for page in range(1):
-        page_number = page + 1
-        proc = Process(target=page_worker_crawler, args=(scene_length, page_number,))
-        proc_list.append(proc)
+    # single process crawling
+    page_worker_crawler(scene_length, scene_per_page)
 
-    for proc in proc_list:
-        proc.start()
-
-    for proc in proc_list:
-        proc.join()
-
-    recorder("movies_info", "total_movies_info.csv",
-             "title,reserve_percent,audience,director,producer,actor_and_actress,genre,release_date\n")
+    recorder("movies_info", "total_movies_info.tsv",
+             "title\treserve_percent\taudience\tdirector\tproducer\tactor_and_actress\tgenre\trelease_date\n")
 
     total_data = []
-    movies_data = glob.glob(TEMP_STORAGE + "/movies_info/*.csv")
+    movies_data = glob.glob(TEMP_STORAGE + "/movies_info/*.tsv")
     for x in movies_data:
         total_data += read("movies_info", x.split("/")[-1]).split('\n')[:-1]
 
     for x in total_data:
-        recorder("movies_info", "total_movies_info.csv", x + "\n")
+        recorder("movies_info", "total_movies_info.tsv", x + "\n")
